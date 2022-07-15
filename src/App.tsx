7bfb5bd9beea1cb6826bc384d810cf10d586cde4
Ghenadie Hilotii <hilotiighenadie0@gmail.com>
2022-07-15 13:44:27 +0000

    added sizes, variants and colors for button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import "./App.css";
+import Button from "./components/Button/Button";
+
+import "./App.scss";
 
 function App() {
-  return <div className="App">Hello React</div>;
+  return (
+    <div className="App">
+      <h3 className="title">Buttons</h3>
+      <p>three types</p>
+      <div className="btn-wrapper">
+        <Button>Default</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="text">Text</Button>
+      </div>
+      <p>three sizes</p>
+      <div className="btn-wrapper">
+        <Button size="sm">Small</Button>
+        <Button size="md">Medium</Button>
+        <Button size="lg">Large</Button>
+      </div>
+      <p>four colors</p>
+      <div className="btn-wrapper">
+        <Button color="default">Default</Button>
+        <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button>
+        <Button color="danger">danger</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
